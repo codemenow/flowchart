dbddf9766edbdc4ca2ab18acfcc639faf9c7547f
Sreekanth Boddireddy <sreekanth.boddireddy@autodesk.com>
2017-08-30 23:12:47 -0700

    poc for flowchart.js

---
+++ app.ts
@@ -0,0 +1,73 @@
+class App {
+    actionBindings(): void {
+        var that = this;
+        $('.create-box').click(function(){
+            that.onCreateBox();
+        });
+        $('.create-circle').click(function(){
+            that.onCreateCircle();
+        });
+        $('.create-line').click(function(){
+            that.onCreateLine();
+        });
+        $('.clear-app').click(function(){
+            that.onClearApp();
+        });
+    }
+    elNamingBinding(id: string): void {
+        $('#'+id).dblclick(function(e) {
+            this.classList.remove('hide-input');
+            this.classList.add('show-input');
+            $('#' + id).find('input').focus();
+        });
+    }
+    onClearApp(): void {
+        $('.app').html('');
+    }
+    createID(): string {
+        return Math.random().toString(36).substring(2);
+    }
+    onCreateBox(): any {
+        let id = this.createID();
+        $('.app').append('<div id="'+id+'" class="el el-box hide-input"><input type="text" name="text"></div>');
+        this.makeDraggable(id);
+        this.elNamingBinding(id);
+        return this;
+    }
+    onCreateCircle(): any {
+        let id = this.createID();
+        $('.app').append('<div id="'+id+'" class="el el-circle hide-input"><input type="text" name="text"></div>');
+        this.makeDraggable(id);
+        this.elNamingBinding(id);
+        return this;
+    }
+    onCreateLine(): any {
+        let id = this.createID();
+        $('.app').append('<div id="'+id+'" class="el el-line hide-input"><input type="text" name="text"></div>');
+        this.makeDraggable(id);
+        this.elNamingBinding(id);
+        return this;
+    }
+    createDiamond(): any {
+        let id = this.createID();
+        $('.app').append('<div id="'+id+'" class="el el-diamond hide-input"><input type="text" name="text"></div>');
+        this.makeDraggable(id);
+        this.elNamingBinding(id);
+        return this;
+    }
+    fillColor(id: string): any {
+        let el = $('#'+ id);
+        el.setAttribute("style", "background-color:"+color);
+        return this;
+    }
+    makeDraggable(id: string): void {
+        let $el = $('#'+ id);
+        $el.draggable();
+        $el.resizable();
+    }
+}
+
+$(document).ready(function () {
+    let app = new App();
+    app.actionBindings();
+});
